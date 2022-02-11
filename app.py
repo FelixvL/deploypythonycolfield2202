46c5b356c5d7907b1debe683dc42985f512b6daa
FelixvL <digitaleservice@gmail.com>
2022-02-11 16:11:20 +0100

    tweede route

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!!!!</p>"
+
+
+@app.route("/tweederoute")
+def hello_world1():
+    return "<p>Hier komt hele andere functie!</p>"
